36220282c764fa7e64212cef7952c25919d6772e
nguyenviethien.1992@gmail.com <nguyenviethien.1992@gmail.com@e21d8a71-0dc3-2054-e88a-89c7a9b8d759>
2013-12-19 19:37:18 +0000

    update

---
+++ trunk/Code/Karaoke/Karaoke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Karaoke
+//
+//  Created by papo on 12/20/13.
+//  Copyright (c) 2013 banana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
