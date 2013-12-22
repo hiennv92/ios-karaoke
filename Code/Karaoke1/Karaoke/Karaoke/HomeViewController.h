859afc2fac65209cfafd8d59dd4f8f96504dc09b
trinhvanduong1292 <trinhvanduong1292@e21d8a71-0dc3-2054-e88a-89c7a9b8d759>
2013-12-22 10:01:04 +0000

    Update home view

--- Code/Karaoke1/Karaoke/Karaoke/HomeViewController.h
+++ Code/Karaoke1/Karaoke/Karaoke/HomeViewController.h
@@ -11,9 +11,20 @@
 @interface HomeViewController : NavibarViewController{
     
 }
+@property (strong, nonatomic) IBOutlet UIView *__bannerView;
+@property (strong, nonatomic) IBOutlet UIView *__introductionVIew;
+@property (strong, nonatomic) IBOutlet UIView *__karaokeClipsView;
+@property (strong, nonatomic) IBOutlet UIView *__singersView;
+@property (strong, nonatomic) IBOutlet UIView *__songsView;
+@property (strong, nonatomic) IBOutlet UIView *__recordsView;
+
+
 
 @property (strong, nonatomic) IBOutlet UIView *__bottomBarView;
+
 @property (strong, nonatomic) IBOutlet UIScrollView *__scrollViewContent;
+@property (strong, nonatomic) IBOutlet UIScrollView *__scrollViewSingers;
+@property (strong, nonatomic) IBOutlet UIScrollView *__scrollViewKaraokeClips;
 
 - (IBAction)singerButtonController:(id)sender;
 - (IBAction)songButtonController:(id)sender;
